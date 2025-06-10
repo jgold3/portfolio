ae95f5fb045c2bdee7d5485ddb4ada2aa35a75af
Josh Goldsmith <joshgold366@gmail.com>
2025-06-09 20:52:50 -0400

    Add skeleton of contact api route

---
+++ src/app/api/contact/route.ts
@@ -0,0 +1,11 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(request: NextRequest) {
+  const { name, email, message } = await request.json();
+  console.log(name, email, message);
+  return NextResponse.json({ message: "Message sent" });
+}
+
+export async function GET() {
+  return NextResponse.redirect(new URL("/404", "http://localhost:3000"));
+}
--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -8,16 +8,25 @@ function ContactForm() {
     email: "",
     message: "",
   });
+  const [isLoading, setIsLoading] = useState(false);
   const isDisabled = !formData.name || !formData.email || !formData.message;
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
 
-  const handleSubmit = (e: React.MouseEvent<HTMLButtonElement>) => {
-    e.preventDefault();
-    console.log(formData);
+  const handleSubmit = async () => {
+    setIsLoading(true);
+
+    const response: { message: string } = await fetch("/api/contact", {
+      method: "POST",
+      body: JSON.stringify(formData),
+    }).then((res) => res.json());
+
+    console.log(response);
+    setIsLoading(false);
   };
+  console.log(isLoading);
 
   return (
     <div className="flex w-full flex-col gap-4 sm:grid sm:max-w-4xl sm:grid-cols-2">
@@ -31,8 +40,8 @@ function ContactForm() {
       />
       <input
         className="w-full rounded-md border border-primary px-4 py-2 font-primary text-primary placeholder:text-primary focus:ring-1 focus:ring-primary focus:outline-none sm:max-w-sm sm:justify-self-end"
-        name="email"
         type="email"
+        name="email"
         placeholder="Email"
         value={formData.email}
         onChange={handleChange}
